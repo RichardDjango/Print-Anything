53b4c94b0498ef952c4e191a1be90ec2b91c89b0
richardgeorgepadua <richardpadua555@gmail.com>
2024-12-17 15:51:25 -0500

    Initial commit

--- src/main/java/PrintAnything.java
+++ src/main/java/PrintAnything.java
@@ -1,5 +1,5 @@
 public class PrintAnything {
     public static void main(String[] args) {
-        System.out.println("My name is Richard");
+        System.out.println("My name is Richard and I am from India");
     }
 }
